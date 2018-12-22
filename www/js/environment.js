c202b2ef81ae9b3a823e799a6e50e79c251a9ae0
2beatakan <45421760+2beatakan@users.noreply.github.com>
2018-12-22 13:07:31 +0100

    Update environment.js

--- www/js/environment.js
+++ www/js/environment.js
@@ -6,7 +6,7 @@ var ENV = {
     //SERVICE_URL: 'http://10.0.0.8:5001',
     //SERVICE_URL: 'http://192.168.1.16:5001',
     //CHAT_SERVER_URL: 'http://192.168.1.16:3000/',
-    SERVICE_URL: 'http://erosify-server.herokuapp.com',
-    CHAT_SERVER_URL: 'https://erosify-chat-server.herokuapp.com/',
+    SERVICE_URL: 'http://137.117.139.114:5000',
+    CHAT_SERVER_URL: 'https://137.117.139.114:3000',
     AMAZON_S3: 'https://erosifyimages.s3.amazonaws.com'
 };
